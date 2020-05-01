e7e22c2eaef5e6fc2181632b71e0e93062423350
Nikita Lorents <suguha10@mail.ru>
2020-05-01 18:14:22 +0600

    Added fx compiler

---
+++ src/main/java/Client.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.net.InetAddress;
+import java.net.Socket;
+
+public class Client {
+
+    public static void main(String[] args) throws FileNotFoundException {
+        int bytesRead;
+        InputStream in;
+        int bufferSize=0;
+
+        try {
+            Socket socket = new Socket(InetAddress.getLocalHost(), 8030);
+            bufferSize=socket.getReceiveBufferSize();
+            in=socket.getInputStream();
+            DataInputStream clientData = new DataInputStream(in);
+            String fileName = clientData.readUTF();
+            System.out.println(fileName);
+            OutputStream output = new FileOutputStream("C:/Users/Power/Downloads/"+ fileName);
+            byte[] buffer = new byte[bufferSize];
+            int read;
+            while((read = clientData.read(buffer)) != -1){
+                output.write(buffer, 0, read);
+            }
+
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/Server.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+
+    public static void main(String[] args) throws IOException {
+        ServerSocket server = new ServerSocket(8030);//запускаем сервер на порту 8030
+        Socket socket = null;
+        socket = server.accept();
+        final File myFile= new File("D:/4game/icons"); //sdcard/DCIM.JPG
+        byte[] mybytearray = new byte[8192];
+        FileInputStream fis = new FileInputStream(myFile);
+        BufferedInputStream bis = new BufferedInputStream(fis);
+        DataInputStream dis = new DataInputStream(bis);
+        OutputStream os;
+        try {
+            //Socket socket = new Socket();
+            os = socket.getOutputStream();
+            DataOutputStream dos = new DataOutputStream(os);
+            dos.writeUTF(myFile.getName());
+            dos.writeLong(mybytearray.length);
+            int read;
+            while((read = dis.read(mybytearray)) != -1){
+                dos.write(mybytearray, 0, read);
+            }
+
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+}
